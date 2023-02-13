2c971b3d0d2fb2e3750f7f250a43d5d2efdb2857
alexveb <verbalex2011@hotmail.com>
2023-02-13 16:33:59 +0200

    finish project style it with Bulma

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,5 @@
 // new props type destructuring title and handle
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description }) {
   //   const title = props.title;
   //   const handle = props.handle;
 
@@ -7,11 +7,21 @@ function ProfileCard({ title, handle, image }) {
   //   const { title, handle } = props;
 
   return (
-    <div>
-      {/* show images with <img src={image} /> */}
-      <img src={image} alt="pda logo" />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          {/* show images with <img src={image} /> */}
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
     </div>
   );
 }
